58735e91be636f1fc599ed86ce837f6c8dcaa7ab
Eunhyuk Kim <gunoodaddy@gmail.com>
2012-01-21 00:40:25 +0900

    BufferedTransport.h function body moved to cpp and moved including Logger.h, Exception.h to cpp

--- src/BufferedTransport.cpp
+++ src/BufferedTransport.cpp
@@ -1,7 +1,98 @@
 #include "Coconut.h"
+#include "Exception.h"
+#include "Logger.h"
 #include "BufferedTransport.h"
 
 namespace coconut { 
+BufferedTransport::BufferedTransport() : readPos_(0) { 
+	LOG_TRACE("BufferedTransport : %p", this);
+}
+
+BufferedTransport::~BufferedTransport() {
+	LOG_TRACE("~BufferedTransport : %p", this);
+}
+
+int BufferedTransport::write(const void* ptr, size_t size) {
+	ScopedMutexLock(lock_);
+	buffer_.append((const char*)ptr, size);
+	return size;
+}
+
+int BufferedTransport::read(std::string &data, size_t size) {
+	ScopedMutexLock(lock_);
+	if(remainingSize() < size) {
+		size = remainingSize();
+	}
+	data.assign((const char *)currentPtr(), size);
+	fastforward(size);
+	return size;
+}
+
+int BufferedTransport::read(void *ptr, size_t size) {
+	ScopedMutexLock(lock_);
+	if(remainingSize() < size) {
+		size = remainingSize();
+	}
+	memcpy(ptr, currentPtr(), size);
+	fastforward(size);
+	return size;
+}
+
+const void * BufferedTransport::peek(size_t &size) {
+	ScopedMutexLock(lock_);
+	if(remainingSize() < size) {
+		size = remainingSize();
+	}
+	return currentPtr();
+}
+
+void BufferedTransport::ackReadSize(size_t size) {
+	fastforward(size);
+}
+
+size_t BufferedTransport::totalSize() {
+	return buffer_.size();
+}
+
+size_t BufferedTransport::remainingSize() {
+	return buffer_.size() - readPos_;
+}
+
+const void * BufferedTransport::basePtr() {
+	return buffer_.c_str();
+}
+
+const void * BufferedTransport::currentPtr() {
+	return buffer_.c_str() + readPos_;
+}
+
+size_t BufferedTransport::readPos() {
+	return readPos_;
+}
+
+void BufferedTransport::clear() {
+	ScopedMutexLock(lock_);
+	readPos_ = 0;
+	buffer_.clear();
+}
+
+void BufferedTransport::rewind(size_t size) {
+	ScopedMutexLock(lock_);
+	int temp = readPos_ - size;
+	if(temp < 0) {
+		throw ProtocolException("readpos less than 0 by rewind");
+	}
+	readPos_ = temp;
+}
+
+void BufferedTransport::fastforward(size_t size) {
+	ScopedMutexLock(lock_);
+	int temp = readPos_ + size;
+	if(temp > (int)buffer_.size())
+		throw ProtocolException("readpos exceed buffer size by fastforward");
+	readPos_ = temp;
+}
+
 
 }
 
--- src/BufferedTransport.h
+++ src/BufferedTransport.h
@@ -1,108 +1,38 @@
 #pragma once
 
 #include <string>
-#include "Exception.h"
 #include "BaseVirtualTransport.h"
-#include "Logger.h"
 #include "ThreadUtil.h"
 
 namespace coconut { 
 
 class COCONUT_API BufferedTransport : public BaseVirtualTransport {
 public:
-	BufferedTransport() : readPos_(0) { 
-		LOG_TRACE("BufferedTransport : %p", this);
-	}
-
-	~BufferedTransport() {
-		LOG_TRACE("~BufferedTransport : %p", this);
-	}
+	BufferedTransport();
+	~BufferedTransport();
 
 public:
 	const char *className() {
 		return "BufferedTransport";
 	}
 	
-	int write(const void* ptr, size_t size) {
-		ScopedMutexLock(lock_);
-		buffer_.append((const char*)ptr, size);
-		return size;
-	}
+	int write(const void* ptr, size_t size);
+	int read(std::string &data, size_t size);
+	int read(void *ptr, size_t size);
+	const void * peek(size_t &size);
+	void ackReadSize(size_t size);
 
-	int read(std::string &data, size_t size) {
-		ScopedMutexLock(lock_);
-		if(remainingSize() < size) {
-			size = remainingSize();
-		}
-		data.assign((const char *)currentPtr(), size);
-		fastforward(size);
-		return size;
-	}
-
-	int read(void *ptr, size_t size) {
-		ScopedMutexLock(lock_);
-		if(remainingSize() < size) {
-			size = remainingSize();
-		}
-		memcpy(ptr, currentPtr(), size);
-		fastforward(size);
-		return size;
-	}
-
-	const void * peek(size_t &size) {
-		ScopedMutexLock(lock_);
-		if(remainingSize() < size) {
-			size = remainingSize();
-		}
-		return currentPtr();
-	}
-
-	void ackReadSize(size_t size) {
-		fastforward(size);
-	}
 public:
-	size_t totalSize() {
-		return buffer_.size();
-	}
+	size_t totalSize();
+	size_t remainingSize();
+	size_t readPos();
 
-	size_t remainingSize() {
-		return buffer_.size() - readPos_;
-	}
+	const void * basePtr();
+	const void * currentPtr();
 
-	const void * basePtr() {
-		return buffer_.c_str();
-	}
-
-	const void * currentPtr() {
-		return buffer_.c_str() + readPos_;
-	}
-
-	size_t readPos() {
-		return readPos_;
-	}
-
-	void clear() {
-		ScopedMutexLock(lock_);
-		readPos_ = 0;
-		buffer_.clear();
-	}
-
-	void rewind(size_t size) {
-		ScopedMutexLock(lock_);
-		int temp = readPos_ - size;
-		if(temp < 0) {
-			throw ProtocolException("readpos less than 0 by rewind");
-		}
-		readPos_ = temp;
-	}
-
-	void fastforward(size_t size) {
-		ScopedMutexLock(lock_);
-		int temp = readPos_ + size;
-		if(temp > (int)buffer_.size())
-			throw ProtocolException("readpos exceed buffer size by fastforward");
-		readPos_ = temp;
-	}
+	void clear();
+	void rewind(size_t size);
+	void fastforward(size_t size);
 
 private:
 	std::string buffer_;
